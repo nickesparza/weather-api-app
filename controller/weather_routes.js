bf6959f138d367bf6956a078ef1295cbe64a7e35
nickesparza <nickesparza@gmail.com>
2022-07-08 21:26:34 -0500

    get weather data displaying

--- controller/weather_routes.js
+++ controller/weather_routes.js
@@ -1,15 +1,25 @@
 const express = require('express')
 // router variable instead of app
 const router = express.Router()
+const fetch = require('node-fetch')
+
+
 
 router.get('/', (req, res) => {
-    res.render('show')
+    res.render('index')
 })
 
-router.get('/weather/78729', (req, res) => {
-    fetch('http://api.openweathermap.org/data/2.5/weather?zip=78729,us&units=imperial&appid=7491e9be98eb7aafc1c275a2ef552378')
-        .then(data => res.json(data))
-        .catch(err => res.json(err))
+router.post('/', async (req, res) => {
+    // console.log(req.body)
+    const targetUrl = `http://api.openweathermap.org/data/2.5/weather?zip=${req.body.zip},us&units=imperial&appid=7491e9be98eb7aafc1c275a2ef552378`
+    const response = await fetch(targetUrl)
+    const body = await response.json()
+    if (body.cod === 200) {
+        body.main.temp = Math.floor(body.main.temp)
+        body.main.feels_like = Math.floor(body.main.feels_like)
+    }
+    // console.log(body)
+    res.render('show', { body: body })
 })
 
 module.exports = router
